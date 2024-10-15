ebe4e5414d2521e196d843d0c6629dbee1a5ad6a
EvgeniyStarikov <tracer2333@mail.ru>
2024-10-15 09:47:02 +0300

    15/10

---
+++ module_2_2.py
@@ -0,0 +1,16 @@
+print ('Введите 3 числа по очереди')
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+if (first==second
+    and second==third
+        and first==third):
+    print(3)
+elif (first==second
+    or second==third
+        or first==third):
+    print(2)
+elif (first!=second
+    and second!=third
+        and first!=third):
+    print(0)
